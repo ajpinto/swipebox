ccf6877ac50374f7f09f18e0d3bb8f1c54b5f6ba
ajpinto <aj@isimplifyme.com>
2013-10-23 14:06:41 -0500

    Update jquery.swipebox.js
    Added support to block related videos from youtube

--- source/jquery.swipebox.js
+++ source/jquery.swipebox.js
@@ -18,6 +18,7 @@
 			hideBarsDelay : 3000,
 			videoMaxWidth : 1140,
 			vimeoColor : 'CCCCCC',
+			youTubeRelated : true, //whether or not to display related vidoes from YouTube once video is finished
 			beforeOpen: null,
 		      	afterClose: null
 		},
@@ -499,8 +500,9 @@
 				var youtubeUrl = url.match(/watch\?v=([a-zA-Z0-9\-_]+)/);
 				var vimeoUrl = url.match(/vimeo\.com\/([0-9]*)/);
 				if( youtubeUrl ){
-
-					iframe = '<iframe width="560" height="315" src="//www.youtube.com/embed/'+youtubeUrl[1]+'" frameborder="0" allowfullscreen></iframe>';
+					if(youTubeRelated) relVid = '';
+					else relVid = '?rel=0'
+					iframe = '<iframe width="560" height="315" src="//www.youtube.com/embed/'+youtubeUrl[1]+relVid+'" frameborder="0" allowfullscreen></iframe>';
 				
 				}else if(vimeoUrl){
 
